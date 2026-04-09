c2695b08337f4ef6a168d3958f0f51cfd61f04d6
Thinan Eshan <thinan.eshan@gmail.com>
2026-04-09 17:09:27 +0000

    Add screenshots for assignment

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-const express = require("express");
-const app = express();
-const PORT = 4000;
-
-app.get("/", (req, res) => {
-  res.send(`
-    <h1>AWS Deployment CI/CD Pipeline Working</h1>
-    <p>Node.js app is running on port 4000</p>
-    <p>Public IP: 34.229.210.53</p>
-  `);
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+res.send(`
+  <h1>CI/CD SUCCESS 🚀</h1>
+  <p>Auto deployed using GitHub Actions</p>
+`);
